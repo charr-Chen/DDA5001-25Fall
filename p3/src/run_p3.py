2b138217fa9f6b9a28d48cea659dbde996f2e411
charr-Chen <1360774917@qq.com>
2025-12-17 23:41:34 +0800

    Add run_p3 script

---
+++ p3/src/run_p3.py
@@ -0,0 +1,50 @@
+import os, subprocess
+
+# Run generation with different temperatures then evaluation
+models = [
+    # {"name": "base", "model": "Qwen/Qwen2.5-Math-1.5B"},
+    {"name": "instruct", "model": "Qwen/Qwen2.5-Math-1.5B-Instruct"},
+]
+
+experiments = [
+    {"dataset": "math", "rollout-n": 16, "temperature": 0.6},
+    # {"dataset": "math", "rollout-n": 16, "temperature": 1.0},
+    # {"dataset": "math", "rollout-n": 16, "temperature": 1.2},
+    # {"dataset": "amc", "rollout-n": 64, "temperature": 0.6},
+    # {"dataset": "amc", "rollout-n": 64, "temperature": 1.0},
+    # {"dataset": "amc", "rollout-n": 64, "temperature": 1.2},
+    # {"dataset": "aime", "rollout-n": 64, "temperature": 0.6},
+    {"dataset": "aime", "rollout-n": 64, "temperature": 1.0},
+    # {"dataset": "aime", "rollout-n": 64, "temperature": 1.2},
+]
+
+for model in models:
+    for exp in experiments:
+        # inference
+        inference_output_dir = f"outputs/inference"
+        os.makedirs(inference_output_dir, exist_ok=True)
+
+        inference_cmd = [
+            "python",
+            "inference.py",
+            f"--model={model['model']}",
+            f"--dataset={exp['dataset']}",
+            f"--temperature={exp['temperature']}",
+            f"--rollout-n={exp['rollout-n']}",
+            f"--output_file={inference_output_dir}/{model['name']}-{exp['dataset']}-{exp['temperature']}.jsonl",
+            f"--batch-size=16",
+            f"--dp-size=1",
+        ]
+        subprocess.run(inference_cmd)
+
+        # evaluation
+        evaluation_output_dir = f"outputs/evaluation"
+        os.makedirs(evaluation_output_dir, exist_ok=True)
+
+        evaluation_cmd = [
+            "python",
+            "evaluate.py",
+            f"--input_file={inference_output_dir}/{model['name']}-{exp['dataset']}-{exp['temperature']}.jsonl",
+            f"--output_file={evaluation_output_dir}/{model['name']}-{exp['dataset']}-{exp['temperature']}-eval.jsonl",
+        ]
+        subprocess.run(evaluation_cmd)
